4efa56cac1f07ddf68f450d5898a372ba8d804fc
Pea Lutz <pea@glocal.coop>
2016-09-28 16:14:01 -0400

    Removed unused required file causing error.

--- jacobin-core-functionality.php
+++ jacobin-core-functionality.php
@@ -32,7 +32,6 @@ require_once( 'includes/lib/class-jacobin-core-taxonomy.php' );
 
 // Load plugin class files
 require_once( 'includes/class-jacobin-core.php' );
-require_once( 'includes/class-jacobin-core-content-filters.php' );
 require_once( 'includes/class-jacobin-core-register-cpt.php' );
 require_once( 'includes/class-jacobin-core-register-fields.php' );
 require_once( 'includes/class-jacobin-core-field-settings.php' );
