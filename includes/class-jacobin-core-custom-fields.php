7d6787d910c172229d3be9ae84931e6b1acb1ce1
Pea <pea@misfist.com>
2017-09-12 11:34:45 -0400

    Removed Interviewer field group

--- includes/class-jacobin-core-custom-fields.php
+++ includes/class-jacobin-core-custom-fields.php
@@ -1562,35 +1562,7 @@
        /**
         * Post Content Custom Fields
         */
-        acf_add_local_field_group( array(
-        	'key' => 'group_interviewer',
-        	'title' => __( 'Interviewer', 'jacobin-core' ),
-        	'fields' => array (
 
-        	),
-        	'location' => array(
-        		array (
-        			array (
-        				'param' => 'post_taxonomy',
-        				'operator' => '==',
-        				'value' => 'format:interview',
-        			),
-        			array (
-        				'param' => 'post_type',
-        				'operator' => '==',
-        				'value' => 'post',
-        			),
-        		),
-        	),
-        	'menu_order' => 0,
-        	'position' => 'normal',
-        	'style' => 'default',
-        	'label_placement' => 'top',
-        	'instruction_placement' => 'label',
-        	'hide_on_screen' => '',
-        	'active' => 1,
-        	'description' => '',
-        ));
 
        /**
         * Post Secondary Image
