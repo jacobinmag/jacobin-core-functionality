77e4cbf9736da9a888cda1c7e24b3624bbdce525
Pea <pea@misfist.com>
2017-10-14 16:24:13 -0400

    Commented out listicle section type
    * #361 Commented out listicle section type @link https://github.com/positiondev/jacobin/issues/361

--- jacobin-core-functionality.php
+++ jacobin-core-functionality.php
@@ -10,7 +10,7 @@
  * Text Domain:     jacobin-core
  * Domain Path:     /languages
  *
- * Version:         0.4.3
+ * Version:         0.4.4
  *
  * @package         Core_Functionality
  */
@@ -55,7 +55,7 @@ require_once( 'admin/class-jacobin-core-admin.php' );
  * @return object Jacobin_Core
  */
 function Jacobin_Core () {
-	$instance = Jacobin_Core::instance( __FILE__, '0.4.3' );
+	$instance = Jacobin_Core::instance( __FILE__, '0.4.4' );
 
 	return $instance;
 }
