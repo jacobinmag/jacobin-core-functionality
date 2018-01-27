2929613de21efbdedeecb0be3db5e2c6a523abf5
Pea <pea@misfist.com>
2018-01-26 19:59:18 -0500

    Create Attachment Post function
    Added attachment post function for when image tag source isn't an attachment post. Once it's added as an attachment post, it is added to the post as a featured image.

--- utils/media-utilities.php
+++ utils/media-utilities.php
@@ -18,57 +18,106 @@
  */
 function jacobin_core_clean_post_images_init( $site = null, $args = array() ) {
 
+  var_dump( $site, $args );
+
   if( is_multisite() && $site ) {
     switch_to_blog( $site );
   }
 
-  if( $posts = jacobin_core_posts_without_featured_image( $args ) ) {
-    $total = count( $posts );
+  // If posts
+  if( $posts = jacobin_core_posts_without_featured_image( $args )  ) {
 
-    echo "\n-----\n{$total} without featured images were found.\n-----\n";
+    $total = count( $posts );
+    $count = 0;
+    $start = wp_count_posts( 'attachment' );
+    $no_image = 0;
+
+    if ( defined( 'WP_CLI' ) && WP_CLI ) {
+    	WP_CLI::log( "\n----------\nPosts without featured image: {$total}\n----------\n" );
+    } else {
+      echo "\n----------\n";
+      echo "Posts without featured image: {$total}";
+      echo "\n----------\n";
+    }
 
     foreach( $posts as $post_id ) {
 
-      echo "\n----------\n";
-      echo "Results for {$post_id}.\n";
+      $count++;
 
+      if ( defined( 'WP_CLI' ) && WP_CLI ) {
+        WP_CLI::log( "\n----------\nPost ID: {$post_id} - {$count} of {$total}\n" );
+      } else {
+        echo "\n----------\n";
+        echo "Post ID: {$post_id} - {$count} of {$total}\n";
+      }
+
+      //If image tag
       if( $url = jacobin_core_find_media_in_post_content( $post_id ) ) {
 
-        echo "Image {$url} was found in post {$post_id}.\n";
+        if ( defined( 'WP_CLI' ) && WP_CLI ) {
+          WP_CLI::success( "Image found: {$url}\n" );
+        } else {
+          echo "Image found: {$url}\n";
+        }
 
+        //If attachment id found
         if( $attachment_id = attachment_url_to_postid( $url ) ) {
 
-          //set_post_thumbnail( $post_id, $attachment_id );
+          if ( defined( 'WP_CLI' ) && WP_CLI ) {
+            WP_CLI::success( "Attachment ID found: {$attachment_id}\n" );
+          } else {
+            echo "Attachment ID found: {$attachment_id}\n";
+          }
 
-          echo "{$url}'s attachment id is {$attachment_id}\n";
+          set_post_thumbnail( $post_id, $attachment_id );
+
+          if ( defined( 'WP_CLI' ) && WP_CLI ) {
+            WP_CLI::success( "Featured image assigned: {$attachment_id}\n" );
+          } else {
+            echo "Featured image assigned: {$attachment_id}\n";
+          }
 
         } else {
 
-          echo "No attachment id was found for {$url} .\n";
+          if ( defined( 'WP_CLI' ) && WP_CLI ) {
+            WP_CLI::log( "No attachment ID found. Creating Post...\n" );
+          } else {
+            echo "No attachment ID found. Creating Post...\n";
+          }
 
-          continue;
+          jacobin_core_add_attachment_post( $url, $post_id );
 
         }
 
+
       } else {
 
-        echo "Post {$post_id} has no image tag.\n";
+        if ( defined( 'WP_CLI' ) && WP_CLI ) {
+          WP_CLI::warning( "Skipping: No image found in post.\n" );
+        } else {
+          echo "Skipping: No image found in post.\n";
+        }
+
+        $no_image++;
 
         continue;
 
       }
 
-      echo "\n----------\n";
-
     }
 
-    echo "\n----------\n";
-    echo "{$total} posts were processed.";
-    echo "\n----------\n";
+    $end = wp_count_posts( 'attachment' );
+    $new = $end->inherit - $start->inherit;
+
+    if ( defined( 'WP_CLI' ) && WP_CLI ) {
+      WP_CLI::log( "\n----------\nPosts processed: {$count} - Posts without images: {$no_image}\n----------\n" );
+    } else {
+      echo "\n----------\nPosts processed: {$count} - Posts without images: {$no_image}\n----------\n";
+    }
 
   } else {
 
-    wp_die( 'No posts without featured images were found.' );
+     wp_die( 'No posts without featured images were found.' );
 
   }
 
@@ -106,9 +155,9 @@ function jacobin_core_posts_without_featured_image( $args = array() ) {
 
   if( $query->have_posts() ) {
 
-    $posts = array();
-
     while( $query->have_posts() ) {
+
+      $total = count( $query->posts );
 
       return $query->posts;
 
@@ -150,8 +199,6 @@ function jacobin_core_find_media_in_post_content( $post_id ) {
 
     if( empty( $image ) ) {
 
-      echo "The first image tag in post {$post_id} doesn't seem to have a `src` attribute.\n";
-
       return false;
     }
 
@@ -163,7 +210,64 @@ function jacobin_core_find_media_in_post_content( $post_id ) {
     return $url;
   }
 
-  echo "Finding an image tag requires DomDocument";
+  else {
 
-  return;
+    wp_die( "Finding an image tag requires DomDocument" );
+
+  }
+
+}
+
+/**
+ * Create Attachment Post
+ *
+ * @param string $url
+ * @param int $parent_id
+ */
+function jacobin_core_add_attachment_post( $url, $parent_id ) {
+  $wp_upload_dir = wp_upload_dir();
+
+  $base_path = implode( '/', array_slice( explode( '/', $url ), -3, 3, true ) );
+  $filename =  trailingslashit( $wp_upload_dir['basedir'] ) . $base_path;
+
+  if( file_exists( $filename ) ) {
+
+    // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+    require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+    $filetype = wp_check_filetype( basename( $filename ), null );
+
+    $attachment = array(
+      'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
+      'post_mime_type' => $filetype['type'],
+      'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+      'post_content'   => '',
+      'post_status'    => 'inherit'
+    );
+
+    $attachment_id = wp_insert_attachment( $attachment, $filename, $parent_id );
+
+    $attachment_data = wp_generate_attachment_metadata( $attachment_id, $filename );
+    wp_update_attachment_metadata( $attachment_id, $attachment_data );
+
+    set_post_thumbnail( $parent_id, $attachment_id );
+
+    if ( defined( 'WP_CLI' ) && WP_CLI ) {
+      WP_CLI::success( "Media post created: {$url} - {$attachment_id}\nFeatured image assigned: {$attachment_id}\n" );
+    } else {
+      echo "Media post created: {$url} - {$attachment_id}\n";
+      echo "Featured image assigned: {$attachment_id}\n";
+    }
+
+  } else {
+
+    if ( defined( 'WP_CLI' ) && WP_CLI ) {
+      WP_CLI::warning( "Media Post not created - File doesn't exist: {$filename}.\n" );
+    } else {
+      echo "Media Post not created - File doesn't exist: {$filename}.\n";
+    }
+
+    return;
+
+  }
 }
