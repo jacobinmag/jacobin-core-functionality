43656d80759fe915b421f6beb742c7ba2332614a
Pea <pea@misfist.com>
2018-02-27 21:48:03 -0500

    #443 Added shortcode for non-admin users to embed scripts
     #443 Added shortcode for non-admin users to embed scripts. @link https://github.com/positiondev/jacobin/issues/443 
      @link https://codex.wordpress.org/Roles_and_Capabilities#unfiltered_html

--- includes/class-jacobin-core-shortcodes.php
+++ includes/class-jacobin-core-shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Jacobin Core Register Shortcodes
- * 
+ *
  * @package    Jacobin_Core
  * @subpackage Jacobin_Core\Includes
  * @since      0.1.3
@@ -27,7 +27,7 @@ class Jacobin_Register_Shortcodes {
         add_action( 'init', array( $this, 'detect_shortcode_ui' ) );
         add_action( 'init', array( $this, 'register_shortcodes' ) );
         add_action( 'init', array( $this, 'register_shortcode_ui' ) );
-        
+
     }
 
     /**
@@ -70,12 +70,14 @@ class Jacobin_Register_Shortcodes {
     public function register_shortcodes() {
         add_shortcode( 'embed-timeline', array( $this, 'embed_timeline_shortcode' ) );
         add_shortcode( 'embed-chart', array( $this, 'embed_chart_shortcode' ) );
+        add_shortcode( 'embed-script', array( $this, 'embed_script_shortcode' ) );
     }
 
     /**
      * Register Shortcode with Shortcode UI
      *
      * @since 0.1.3
+     * @since 0.4.14
      *
      * @param string $shortcode_tag
      * @param function $shortcode_function
@@ -84,6 +86,39 @@ class Jacobin_Register_Shortcodes {
     public function register_shortcode_ui() {
         add_action( 'register_shortcode_ui', array( $this, 'embed_timeline_shortcode_ui' ) );
         add_action( 'register_shortcode_ui', array( $this, 'embed_chart_shortcode_ui' ) );
+        add_action( 'register_shortcode_ui', array( $this, 'embed_script_shortcode_ui' ) );
+    }
+
+    /**
+     * Callback for the `embed_script` shortcode
+     * It renders the shortcode based on supplied attributes.
+     *
+     * @example `[embed-script src={url} defer=defer]`
+     *
+     * @since 0.4.14
+     *
+     * @param string $atts
+     * @param string $content
+     * @param string $shortcode_tag
+     */
+    public function embed_script_shortcode( $atts, $content, $shortcode_tag ) {
+
+    	$atts = shortcode_atts(
+    		array(
+    			'src' => '',
+    			'defer' => 'defer',
+    			'type' => 'text/javascript',
+    		),
+    		$atts,
+    		$shortcode_tag
+    	);
+
+    	return sprintf( '<script scr="%s" defer="%s" type="%s"></script>',
+    	    esc_url( $atts['src'] ),
+    	    esc_attr( $atts['defer'] ),
+    	    esc_attr( $atts['type'] )
+    	);
+
     }
 
     /**
@@ -108,7 +143,7 @@ class Jacobin_Register_Shortcodes {
 
         // Output buffering here.
         ob_start();
-        
+
         include_once ( 'views/embed-timeline.php' );
 
         return ob_get_clean();
@@ -136,7 +171,7 @@ class Jacobin_Register_Shortcodes {
 
         // Output buffering here.
         ob_start();
-        
+
         include_once ( 'views/embed-chart.php' );
 
         return ob_get_clean();
@@ -199,6 +234,34 @@ class Jacobin_Register_Shortcodes {
         );
 
         shortcode_ui_register_for_shortcode( 'embed-chart', $shortcode_ui_args );
+    }
+
+    /**
+     * Embed Script Shortcode UI
+     *
+     * @since 0.4.14
+     *
+     */
+    public function embed_script_shortcode_ui() {
+        $fields = array(
+            array(
+                'label'    => esc_html__( 'Script URL', 'jacobin-core' ),
+                'attr'     => 'src',
+                'type'     => 'url',
+            ),
+        );
+
+        /*
+         * Define the Shortcode UI arguments.
+         */
+        $shortcode_ui_args = array(
+            'label'         => esc_html__( 'Embed Script', 'jacobin-core' ),
+            'listItemImage' => 'dashicons-editor-code',
+            'post_type'     => array( 'post' ),
+            'attrs'         => $fields,
+        );
+
+        shortcode_ui_register_for_shortcode( 'embed-script', $shortcode_ui_args );
     }
 
 }
