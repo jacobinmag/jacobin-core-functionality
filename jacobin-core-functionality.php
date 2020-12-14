4fa3477b996ef46fdffa26e59fa34d798f446ddc
Pea <pea@misfist.com>
2020-12-14 14:16:34 -0500

    Bump 0.5.14

--- jacobin-core-functionality.php
+++ jacobin-core-functionality.php
@@ -10,7 +10,7 @@
  * Text Domain:     jacobin-core
  * Domain Path:     /languages
  *
- * Version:         0.5.13
+ * Version:         0.5.14
  *
  * @package         Core_Functionality
  */
@@ -60,7 +60,7 @@ require_once( 'integrations/wp-cli.php' );
  * @return object Jacobin_Core
  */
 function Jacobin_Core () {
-	$instance = Jacobin_Core::instance( __FILE__, '0.5.13' );
+	$instance = Jacobin_Core::instance( __FILE__, '0.5.14' );
 
 	return $instance;
 }
