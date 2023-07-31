3857f4a0c51f61cc98ad27867ba6a6ebd060b7c1
Pea <pea@misfist.com>
2023-07-31 17:22:27 -0400

    Remove unused

--- includes/class-jacobin-core-register-routes.php
+++ includes/class-jacobin-core-register-routes.php
@@ -100,48 +100,6 @@ class Jacobin_Rest_API_Routes {
 			)
 		);
 
-		// register_rest_route( $this->namespace, '/guest-author', array(
-		// 'methods'     => 'GET',
-		// 'callback'    => array( $this, 'get_guest_author' ),
-		// 'args' => array(
-		// 'slug' => array(
-		// 'description' => esc_html__( 'The author term slug parameter is used to retrieve a guest author', 'jacobin-core' ),
-		// 'type'        => 'string',
-		// 'validate_callback' => function( $param, $request, $key ) {
-		// return ( is_string( $param ) );
-		// }
-		// ),
-		// 'term_id' => array(
-		// 'description' => esc_html__( 'The author term ID parameter is used to retrieve a guest author', 'jacobin-core' ),
-		// 'type'        => 'number',
-		// 'validate_callback' => function( $param, $request, $key ) {
-		// return ( is_numeric( $param ) );
-		// }
-		// ),
-		// 'id' => array(
-		// 'description' => esc_html__( 'The guest author id parameter is used to retrieve a guest author', 'jacobin-core' ),
-		// 'type'        => 'number',
-		// 'validate_callback' => function( $param, $request, $key ) {
-		// return ( is_numeric( $param ) && 'guest-author' == get_post_type( $param ) );
-		// }
-		// ),
-		// ),
-		// 'permission_callback' => '__return_true',
-		// ) );
-
-		// register_rest_route( $this->namespace, '/guest-author/(?P<id>\d+)', array(
-		// 'methods'     => 'GET',
-		// 'callback'    => array( $this, 'get_guest_author' ),
-		// 'args' => array(
-		// 'term_id' => array(
-		// 'validate_callback' => function( $param, $request, $key ) {
-		// return ( is_numeric( $param ) );
-		// }
-		// ),
-		// ),
-		// 'permission_callback' => '__return_true',
-		// ) );
-
 		register_rest_route(
 			$this->namespace,
 			'/featured-content/(?P<slug>[a-zA-Z0-9-]+)',
